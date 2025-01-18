ec52efcd3ece7969b4e1dc9311feb242e7e30213
Krishjot8 <krishjot8@yahoo.com>
2025-01-18 15:12:03 -0600

    handled registration errors

--- src/app/components/account/register/register.component.ts
+++ src/app/components/account/register/register.component.ts
@@ -13,70 +13,102 @@ export class RegisterComponent implements OnInit {
   registrationForm: FormGroup = new FormGroup({});
   submitted = false;
 
-  constructor( private router: Router, private fb: FormBuilder, private registerService: RegisterService)
-  {
-
-    this.registrationForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      username: ['',[Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      dateOfBirth:['',[Validators.required]],
-      phoneNumber: ['', [Validators.pattern(/^\+?[1-9]\d{0,14}$/)]],
-      password: ['', [Validators.required, Validators.minLength(6),
-        Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/)
-      ]],
-      confirmPassword: ['', [Validators.required]],
-      subscribeToNewsLetter:[false]
-    },
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private registerService: RegisterService
+  ) {
+    this.registrationForm = this.fb.group(
       {
-        validators: this.passwordMatchValidator
-      });
+        firstName: ['', [Validators.required,
+          Validators.maxLength(45),
+          Validators.pattern(/^[A-Za-z' ]+([- ][A-Za-z' ]+)*( (IV|V|VI|VII|VIII|IX|X|XI|XII))?$/)]],
+        lastName:  ['', [Validators.required,
+          Validators.maxLength(45),
+          Validators.pattern(/^[A-Za-z' ]+([- ][A-Za-z' ]+)*( (IV|V|VI|VII|VIII|IX|X|XI|XII))?$/)]],
+   username: ['', [Validators.required,Validators.minLength(5),Validators.maxLength(20)]],
+        email: ['', [Validators.required, Validators.email]],
+        dateOfBirth: ['', [Validators.required]],
 
+        phoneNumber: ['', [Validators.pattern(/^(\+?\d{1,3}[-. ]?)?(\(?\d{1,4}\)?[-. ]?)?(\d{1,4}[-. ]?)?(\d{1,4}[-. ]?)?(\d{1,9})$/)]],
 
+        password: ['', [ Validators.required, Validators.minLength(6),
+            Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/),
+          ],
+        ],
+
+        confirmPassword: ['', [Validators.required]],
+        subscribeToNewsLetter: [false],
+      },
+      {
+        validators: this.passwordMatchValidator,
+
+      }
+    );
   }
 
   ngOnInit(): void {}
 
+  private passwordMatchValidator(formGroup: FormGroup) {
 
+    const passwordControl = formGroup.get('password');
+    const confirmPasswordControl = formGroup.get('confirmPassword');
 
-private passwordMatchValidator (form: FormGroup){
-return form.get('password')?.value === form.get('confirmPassword')?.value
-?null: {mismatch:true};
+    if(!passwordControl || !confirmPasswordControl){
 
-}
+return null;
 
+    }
+    if(confirmPasswordControl.errors && !confirmPasswordControl.errors['mismatch']){
 
+return null;
+    }
 
+    if (passwordControl.value !== confirmPasswordControl.value) {
+      confirmPasswordControl.setErrors({ mismatch: true }); // Set the error on confirmPassword
+    } else {
+      confirmPasswordControl.setErrors(null); // Clear the error if passwords match
+    }
+
+    return null;
+  }
 
   onSubmit(): void {
 
-if(this.registrationForm.valid){
+    this.submitted = true;
+    
+    if (this.registrationForm.valid) {
+      const registrationData: UserRegistration = {
+        firstName: this.registrationForm.value.firstName,
+        lastName: this.registrationForm.value.lastName,
+        username: this.registrationForm.value.username,
+        email: this.registrationForm.value.email,
+        dateOfBirth: this.registrationForm.value.dateOfBirth,
+        phoneNumber: this.registrationForm.value.phoneNumber,
+        password: this.registrationForm.value.password,
+        confirmPassword: this.registrationForm.value.confirmPassword,
+        subscribeToNewsLetter: false,
+      };
 
-  const registrationData: UserRegistration = {
-    firstName: this.registrationForm.value.firstName,
-    lastName: this.registrationForm.value.lastName,
-    username:  this.registrationForm.value.username,
-    email:  this.registrationForm.value.email,
-    dateOfBirth: this.registrationForm.value.dateOfBirth ,
-    phoneNumber: this.registrationForm.value.phoneNumber,
-    password: this.registrationForm.value.password,
-    confirmPassword:this.registrationForm.value.confirmPassword,
-    subscribeToNewsLetter: false
-  };
+      console.log('Form Submitted', this.registrationForm.value);
 
-  this.registerService.registerUser(registrationData).subscribe({
-    next: (response) => {
-      console.log('Registration successful', response);
-      // Handle success (e.g., redirect to login)
-    },
-    error: (error) => {
-      console.error('Registration failed', error);
-      // Handle error (e.g., show error message)
+
+      this.registerService.registerUser(registrationData).subscribe({
+        next: (response) => {
+          console.log('Registration successful', response);
+          // Handle success (e.g., redirect to login)
+          this.registrationForm.reset();
+          this.submitted = false;
+        },
+        error: (error) => {
+          console.error('Registration failed', error);
+          // Handle error (e.g., show error message)
+
+        },
+      });
+    } else{
+
+      this.registrationForm.markAllAsTouched();
     }
-  });
-
-}
-
   }
 }
