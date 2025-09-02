9bb2d275a82ee24d3af5e4699da4c7be6a71bac8
Krishjot Sarai <krishjot8@yahoo.com>
2025-09-01 19:11:35 -0500

    yes

---
+++ Services/Authentication/Token/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using Amazon_eCommerce_API.Models.Users;
+
+namespace Amazon_eCommerce_API.Services
+{
+    public interface ITokenService
+    {
+
+          public string GenerateToken(User user);
+    }
+}
