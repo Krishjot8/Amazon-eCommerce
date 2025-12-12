21b35d3af1247ad79b7ebad38443e1e35b4ddfe8
Krishjot8 <krishjot8@yahoo.com>
2025-12-11 20:40:29 -0600

    Added additional DTO's for Seller User Account Registration

---
+++ Models/DTO's/Accounts/SellerUserAccount/SellerRegistration/SellerUserPaymentInformationDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Amazon_eCommerce_API.Models.DTO_s.Accounts.SellerUserAccount.SellerRegistration
+{
+    public class SellerUserPaymentInformationDto
+    {
+
+        [Required]
+        public string CardNumber { get; set; }
+
+    }
+}
--- Models/DTO's/Accounts/SellerUserAccount/SellerRegistration/SellerUserPrimaryContactInformationDto.cs
+++ Models/DTO's/Accounts/SellerUserAccount/SellerRegistration/SellerUserPrimaryContactInformationDto.cs
@@ -1,7 +1,77 @@
-﻿namespace Amazon_eCommerce_API.Models.DTO_s.Accounts.SellerUserAccount.SellerRegistration
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Amazon_eCommerce_API.Models.DTO_s.Accounts.SellerUserAccount.SellerRegistration
 {
     public class SellerUserPrimaryContactInformationDto
     {
+
+
+        public string FirstName { get; set; }
+
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public string CountryOfCitizenship { get; set; }
+
+
+        public string CountryOfBirth { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+
+        public string IdentityDocumentType { get; set; }
+
+       public string IdentityDocumentNumber { get; set; }
+
+
+        public string CountryOfIssue { get; set; }
+
+
+       public DateTime IdentityDocumentExpirationDate { get; set; }
+
+
+
+
+        [Required]
+        public string Country { get; set; }  //Prefilled
+
+
+        [Required]
+
+        public string AddressLine1 { get; set; }
+
+
+        public string? AddressLine2 { get; set; }
+
+
+        [Required]
+
+        public string City { get; set; }
+
+
+        [Required]
+        public string State { get; set; }
+
+
+        [Required]
+
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string ContactPhone { get; set; }
+
+
+        [Required]
+        public bool PointOfContactIsBusinessOwner {  get; set; }
+
+        public bool PointOfContactIsLegalRepresentative { get; set; } // If not benificial Owner
+
+
+        public bool PrimaryContactIsBusinessOwner { get; set; }
+
 
 
     }
